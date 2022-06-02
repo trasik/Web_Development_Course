db125d21f4f159a9aa87d5395ee8c0280df13343
Torendra Rasik <torendrarasik@gmail.com>
2022-06-02 14:15:13 -0400

    Started working on React projects

---
+++ Web Development/Projects - React/client/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Testing Testing...</p>
+    </div>
+  );
+}
+
+export default App;
